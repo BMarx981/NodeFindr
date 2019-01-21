154b21210c0ec8cf8fdc6bde3bc942080bde038e
Brian Marx <brian.marx@expersolve.com>
2019-01-21 15:31:34 -0500

    initial commit

---
+++ src/application/NodeFindrController.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class NodeFindrController implements Initializable {
+	
+	@FXML Label lineLabel = new Label();
+	@FXML TextArea ta1 = new TextArea();
+	@FXML TextArea ta2 = new TextArea();
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {}
+	
+	
+	
+	
+}
