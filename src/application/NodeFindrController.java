495fade655df897473e3eb86225dda552ce76ae5
Brian Marx <brian.marx@expersolve.com>
2019-01-22 10:06:37 -0500

    Lots of updates to begin the project.

--- src/application/NodeFindrController.java
+++ src/application/NodeFindrController.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import org.apache.commons.io.FileUtils;
@@ -11,7 +14,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -20,12 +26,19 @@ public class NodeFindrController implements Initializable {
 	@FXML Label lineLabel = new Label();
 	@FXML TextArea ta1 = new TextArea();
 	@FXML TextArea ta2 = new TextArea();
+	@FXML TextField tf = new TextField();
 	@FXML Button fileSelect = new Button();
+	@FXML Button analyze = new Button();
+	@FXML MenuBar menuBar = new MenuBar();
+	@FXML MenuItem saveItem = new MenuItem();
+	@FXML MenuItem clearItem = new MenuItem(); 
 	
-	String fileName = new String();
+	private String fileName = new String();
+	private String searchNode = new String();
 	
 	@Override
-	public void initialize(URL location, ResourceBundle resources) {}
+	public void initialize(URL location, ResourceBundle resources) {
+	}
 	
 	public void ta1Entered(ActionEvent e) {
 		ta1.getText();
@@ -33,6 +46,10 @@ public class NodeFindrController implements Initializable {
 	
 	public void ta2Entered(ActionEvent e) {
 		ta2.getText();
+	}
+	
+	public void tfEntered(ActionEvent e) {
+		searchNode = tf.getText();
 	}
 	
 	public void fileButtonPressed(ActionEvent e) {
@@ -44,9 +61,29 @@ public class NodeFindrController implements Initializable {
 		}
 	}
 	
-	private String processXMLInput(String fileName) {
+	private void processSelectedFile(String fileName) {
+		FileReader fileReader;
+		try {
+			fileReader = new FileReader(fileName);
+			BufferedReader buffer = new BufferedReader(fileReader);
+			String line = buffer.readLine();
+			ArrayList<String> allNodes = new ArrayList<String>();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		return fileName;
 	}
+	
+	/************************** Menu bar items ***********************************/
+	public void saveItemPressed(ActionEvent e) {
+		
+	}
+	
+	public void clearItemPressed(ActionEvent e) {
+		
+	}
+
 	
 }
